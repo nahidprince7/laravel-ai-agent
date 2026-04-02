4dbfe8a33c91b80c9836c1b6e26e2ec6f5f4f7cf
Nahid <nahid.hasan@dreamonline.co.jp>
2026-04-02 13:48:15 +0600

    migrations models factories & seeders

---
+++ Source/app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'items',
+        'total_amount',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'items' => 'array',
+        'total_amount' => 'decimal:2',
+    ];
+}
